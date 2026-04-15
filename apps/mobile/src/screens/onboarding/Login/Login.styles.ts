035406248c05f3a1ed612e9fd1ade4a18b0305c9
Nico <josenicolasbarrosogarcia@gmail.com>
2026-04-16 00:18:53 +0100

    chore(refactor): moved to onboarding folder

--- apps/mobile/src/screens/onboarding/Login/Login.styles.ts
+++ apps/mobile/src/screens/onboarding/Login/Login.styles.ts
@@ -1,38 +1,20 @@
 import { StyleSheet } from 'react-native';
 
-import { colors } from '../../theme/colors';
-import { fonts } from '../../theme/fonts';
+import { colors } from '@theme/colors';
+import { fonts } from '@theme/fonts';
 
 export const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: colors.background,
   },
-  keyboardView: {
-    flex: 1,
-  },
+
   container: {
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 60,
     width: '100%',
     gap: 25,
-  },
-  header: {
-    alignItems: 'center',
-    paddingBottom: 30,
-  },
-  title: {
-    fontFamily: fonts.family.title.bold,
-    fontSize: fonts.size.xl,
-    color: colors.brown,
-    marginBottom: 6,
-  },
-  subtitle: {
-    fontFamily: fonts.family.primary.regular,
-    fontSize: fonts.size.md,
-    color: colors.brownMid,
-    marginBottom: 8,
   },
   errorText: {
     fontFamily: fonts.family.primary.regular,
@@ -47,9 +29,10 @@ export const styles = StyleSheet.create({
     marginTop: 6,
   },
   link: {
-    color: colors.salmon,
+    color: colors.brownMid,
     fontFamily: fonts.family.primary.regular,
     fontSize: fonts.size.md,
     textAlign: 'center',
+    textDecorationLine: 'underline',
   },
 });
