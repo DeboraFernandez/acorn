f26bf1f6f5f70faebeca25138b66c21b70266781
Nico <josenicolasbarrosogarcia@gmail.com>
2026-04-16 08:45:17 +0100

    fix(index): moved dev dependencies to pull to main.

--- apps/mobile/app/(app)/index.tsx
+++ apps/mobile/app/(app)/index.tsx
@@ -4,7 +4,7 @@ import type { Session } from '@supabase/supabase-js';
 import HomeScreen from '@screens/Home/Home';
 import { supabase } from '@lib/supabase/client';
 import { View } from 'react-native';
-import SignOutButton from 'components/SignOut';
+
 
 export default function HomeRoute() {
   const router = useRouter();
@@ -19,13 +19,13 @@ export default function HomeRoute() {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <SignOutButton />
-      {/* <HomeScreen
-          userName={session?.user.email ?? 'Usuario'}
-          sharedUrl={sharedUrl}
-          onSharedUrlHandled={() => setSharedUrl(null)}
-          onSearchPress={() => router.push('/(app)/search')}
-        />*/}
+
+      <HomeScreen
+        userName={session?.user.email ?? 'Usuario'}
+        sharedUrl={sharedUrl}
+        onSharedUrlHandled={() => setSharedUrl(null)}
+        onSearchPress={() => router.push('/(app)/search')}
+      />
     </View>
   );
 }
