8ddd7e25cc1a94dac8dc7f78e540fc95d25caa08
Nico <josenicolasbarrosogarcia@gmail.com>
2026-04-22 19:46:32 +0100

    fix(profile): simplify consequences list and center bullet text in delete account screen

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -56,11 +56,12 @@ function AuthGate() {
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    CabinetGrotesk: require('../assets/fonts/CabinetGrotesk-Variable.ttf'),
-    Satoshi: require('../assets/fonts/Satoshi-Variable.ttf'),
-    'Satoshi-Regular': require('../assets/fonts/Satoshi/Satoshi-Regular.otf'),
-    'Satoshi-Medium': require('../assets/fonts/Satoshi/Satoshi-Medium.otf'),
-    'CabinetGrotesk-Bold': require('../assets/fonts/CabinetGrotesk/CabinetGrotesk-Bold.otf'),
+    CabinetGrotesk: require('@assets/fonts/CabinetGrotesk-Variable.ttf'),
+    Satoshi: require('@assets/fonts/Satoshi-Variable.ttf'),
+    'Satoshi-Regular': require('@assets/fonts/Satoshi/Satoshi-Regular.otf'),
+    'Satoshi-Medium': require('@assets/fonts/Satoshi/Satoshi-Medium.otf'),
+    'Satoshi-Bold': require('@assets/fonts/Satoshi/Satoshi-Bold.otf'),
+    'CabinetGrotesk-Bold': require('@assets/fonts/CabinetGrotesk/CabinetGrotesk-Bold.otf'),
   });
 
   useEffect(() => {
